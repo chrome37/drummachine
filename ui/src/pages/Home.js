d36c00ec36fb606657cf198b3da14dc87e725cbc
takumi.hongo <takumi.hongo@ibm.com>
2019-09-25 23:24:47 +0900

    add pads

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,25 +1,74 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid'
 
-const useStyle = makeStyles({
+const useStyles = makeStyles({
     pad: {
-
+        width: "100%",
+        height: "auto",
+        '&::before': {
+            content: '""',
+            display: "block",
+            paddingTop: "100%"
+        }
     }
 })
 
 const Pad = (props) => {
-    return (<Button />)
+    const classes = useStyles();
+    const {padName} = props;
+
+    const handleClick = () => {
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === padName.toLowerCase()) {
+            console.log(e.key);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+    });
+
+    return (<Button onClick={() => {handleClick()}} variant='contained' className={classes.pad}>{padName}</Button>)
 }
 
 const Pads = (props) => {
-    
+    const padNames = ["1", "2", "3", "4", "Q", "W", "E", "R", "A", "S", "D", "F", "Z", "X", "C", "V"]
+
+    const padArr = padNames.map(padName => {
+        return (
+            <Grid item md={3} sm={3} xs={3} key={padName}>
+                <Pad padName={padName} />
+            </Grid>
+        )
+    });
+
+    return (
+        <Grid container spacing={1}>
+                {padArr}
+        </Grid>
+    )
 }
 
 const Home = (props) => {
     return (
         <div>
-            <Pad />
+            <Grid container alignItems="stretch" direction="row">
+                <Grid item md={4} >
+                </Grid>
+                <Grid item md={4}>
+                    <Pads/>
+                </Grid>
+                <Grid item md={4}>
+                </Grid>
+            </Grid>
         </div>
         )
 }
