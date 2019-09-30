a4253a9baaabeed8d9c1928f606a321f3781b8cb
takumi.hongo <takumi.hongo@ibm.com>
2019-09-30 16:18:08 +0900

    コンポーネント分割

---
+++ ui/src/components/Pads.js
@@ -0,0 +1,73 @@
+import React, {useEffect} from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid'
+
+const useStyles = makeStyles({
+    pad: {
+        width: "100%",
+        height: "auto",
+        '&::before': {
+            content: '""',
+            display: "block",
+            paddingTop: "100%"
+        }
+    },
+    pads: {
+
+    },
+    displayBox: {
+        height: "270px",
+    }
+})
+
+const Pad = (props) => {
+    const classes = useStyles();
+    const {padName} = props;
+
+    const handleClick = () => {
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === padName.toLowerCase()) {
+            console.log(padName);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+    });
+
+    return (
+            <Button onClick={() => {handleClick()}} 
+                    variant='contained' 
+                    className={classes.pad}>
+                    {padName}
+            </Button>
+            )
+}
+
+const Pads = (props) => {
+    const classes = useStyles();
+    const padNames = ["1", "2", "3", "4", "Q", "W", "E", "R", "A", "S", "D", "F", "Z", "X", "C", "V"]
+
+    const padArr = padNames.map(padName => {
+        return (
+            <Grid item md={3} sm={3} xs={3} key={padName}>
+                <Pad padName={padName} />
+            </Grid>
+        )
+    });
+
+    return (
+        <Grid container spacing={1} className={classes.pads}>
+                {padArr}
+        </Grid>
+    )
+}
+
+export default Pads;
--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,74 +1,25 @@
-import React, {useState, useEffect} from 'react';
-import Button from '@material-ui/core/Button';
+import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box';
 import AppHeader from '../components/AppHeader';
+import Pads from '../components/Pads';
 
 const useStyles = makeStyles({
-    pad: {
-        width: "100%",
-        height: "auto",
-        '&::before': {
-            content: '""',
-            display: "block",
-            paddingTop: "100%"
-        }
+    displayBox: {
+        height: "270px",
     }
-})
+});
 
-const Pad = (props) => {
+const Home = () => {
     const classes = useStyles();
-    const {padName} = props;
-
-    const handleClick = () => {
-    }
-
-    const handleKeyDown = (e) => {
-        if (e.key === padName.toLowerCase()) {
-            console.log(padName);
-        }
-    }
-
-    useEffect(() => {
-        window.addEventListener('keydown', handleKeyDown);
-
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-        }
-    });
-
-    return (
-            <Button onClick={() => {handleClick()}} 
-                    variant='contained' 
-                    className={classes.pad}>
-                    {padName}
-            </Button>
-            )
-}
-
-const Pads = (props) => {
-    const padNames = ["1", "2", "3", "4", "Q", "W", "E", "R", "A", "S", "D", "F", "Z", "X", "C", "V"]
-
-    const padArr = padNames.map(padName => {
-        return (
-            <Grid item md={3} sm={3} xs={3} key={padName}>
-                <Pad padName={padName} />
-            </Grid>
-        )
-    });
-
-    return (
-        <Grid container spacing={1}>
-                {padArr}
-        </Grid>
-    )
-}
-
-const Home = (props) => {
     return (
         <div>
             <AppHeader title="Dram Machine"/>
             <Grid container>
+                <Grid item md={12} sm={12} xs={12}>
+                    <Box className={classes.displayBox}></Box>
+                </Grid>
                 <Grid item md={4} sm={3} xs={1}>
                 </Grid>
                 <Grid item md={4} sm={6} xs={10}>
