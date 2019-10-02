cefeb95863ae6c8e6d7e6fc1061aa0f50632aa24
takumi.hongo <takumi.hongo@ibm.com>
2019-10-02 14:51:24 +0900

    音源 api

--- server/data-access/client/cos-client.js
+++ server/data-access/client/cos-client.js
@@ -9,8 +9,24 @@ class CosClient {
     async getBuckets() {
         return new Promise((resolve, reject) => {
             this.cos.listBuckets((err, data) => {
-                err ? reject(data) : resolve(data);
+                err ? reject(err) : resolve(data);
             });
+        })
+    }
+
+    async getBucketContentsList(bucketName) {
+        return new Promise((resolve, reject) => {
+            this.cos.listObjectsV2({Bucket: bucketName}, (err, data) => {
+                err ? reject(err) : resolve(data);
+            })
+        });
+    }
+
+    async getBucketContent(bucketName, itemName) {
+        return new Promise((resolve, reject) => {
+            this.cos.getObject({Bucket: bucketName, Key: itemName}, (err, data) => {
+                err ? reject(err) : resolve(data);
+            })
         })
     }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,16 +3,56 @@ var router = express.Router();
 const client = require('../data-access/client/mongo-client');
 const passport = require('../utils/verifyToken');
 const cosClient = require('../data-access/client/cos-client');
+const config = require('config');
 
 router.post('/', async (req, res) => {
     const result = await client.createUser(req.body);
     res.json(result);
 });
 
-router.get('/kit', async (req, res) => {
-    const result = await cosClient.getBuckets();
-    console.log(result);
-    res.json(result);
+router.get('/:userId/kits', passport.authenticate('jwt', {session: false}),
+    async (req, res) => {
+        const userId = req.params.userId;
+        const result = await cosClient.getBuckets();
+        res.json(result);
 });
+
+router.get('/:userId/kits/:kitName', passport.authenticate('jwt', {session: false}), 
+    async (req, res) => {
+        const userId = req.params.userId;
+        const kitName = req.params.kitName;
+        const result = await cosClient.getBucketContentsList(kitName).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+        console.log(result);
+        const endpoint = config.cos.config.endpoint;
+        const contents = result.Contents.map(item => {
+            return {
+                pad: "",
+                path:  new URL(`https://${endpoint}/${kitName}/${item.Key}`),
+                name: item.Key
+            }
+        })
+        const resData = {
+            kitName: kitName,
+            contents: contents
+        }
+        res.json(resData);
+    }   
+);
+
+router.get('/:userId/kits/:kitName/:sampleName', passport.authenticate('jwt', {session: false}), 
+    async (req, res) => {
+        const userId = req.params.userId;
+        const kitName = req.params.kitName;
+        const sampleName = req.params.sampleName;
+        const result = await cosClient.getBucketContent(kitName, sampleName).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+        res.send(result.Body);
+    }
+)
 
 module.exports = router;
